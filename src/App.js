7e1625beee25a55f2bac158b843b0af0766f4c8a
Valentin <razumny.valentin@gmail.com>
2025-04-20 15:55:07 +0300

    add useState

--- src/App.js
+++ src/App.js
@@ -7,26 +7,27 @@ import PizzaBlock from "./components/PizzaBlock";
 function App() {
   return (
     <div className="App">
-      <h1>React Pizza</h1>
-      <div class="wrapper">
+      <div className="wrapper">
         <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
               <Categories />
               <Sort />
             </div>
-            <h2 class="content__title">Все пиццы</h2>
-            <div class="content__items">
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
               <PizzaBlock
                 title="Чизбургер-пицца"
                 price="395"
-                imageUrl="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+                imageUrl={
+                  "https://media.dodostatic.net/image/r:292x292/019591c69fac7921a27e4ecd8c99f9df.avif"
+                }
               />
               <PizzaBlock
                 title="Пепперони"
                 price="290"
-                imageUrl="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+                imageUrl="https://media.dodostatic.net/image/r:292x292/019591c69fac7921a27e4ecd8c99f9df.avif"
               />
             </div>
           </div>
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,26 +1,33 @@
-function PizzaBlock(props) {
+import React, { useState } from "react";
+
+function PizzaBlock({ title, price, imageUrl }) {
+  const [countPizza, setCountPizza] = useState(0);
+
+  function addPizza() {
+    setCountPizza(countPizza + 1);
+  }
+
   return (
-    <div class="pizza-block">
-      <img
-        class="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-      />
-      <h4 class="pizza-block__title">{props.title}</h4>
-      <div class="pizza-block__selector">
+    <div className="pizza-block">
+      <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
+      <h4 className="pizza-block__title">{title}</h4>
+      <div className="pizza-block__selector">
         <ul>
-          <li class="active">тонкое</li>
+          <li className="active">тонкое</li>
           <li>традиционное</li>
         </ul>
         <ul>
-          <li class="active">26 см.</li>
+          <li className="active">26 см.</li>
           <li>30 см.</li>
           <li>40 см.</li>
         </ul>
       </div>
-      <div class="pizza-block__bottom">
-        <div class="pizza-block__price">от {props.price} ₽</div>
-        <div class="button button--outline button--add">
+      <div className="pizza-block__bottom">
+        <div className="pizza-block__price">от {price} ₽</div>
+        <button
+          onClick={addPizza}
+          className="button button--outline button--add"
+        >
           <svg
             width="12"
             height="12"
@@ -34,8 +41,8 @@ function PizzaBlock(props) {
             />
           </svg>
           <span>Добавить</span>
-          <i>2</i>
-        </div>
+          <i>{countPizza}</i>
+        </button>
       </div>
     </div>
   );
