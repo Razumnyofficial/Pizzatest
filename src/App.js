537682d630ceebd94b0be7b7e6517afb8854238f
Valentin <razumny.valentin@gmail.com>
2025-04-20 12:31:51 +0300

    first commmit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Pizza</h1>
+      <div class="wrapper">
+        <Header />
+        <div class="content">
+          <div class="container">
+            <div class="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 class="content__title">Все пиццы</h2>
+            <div class="content__items">
+              <PizzaBlock
+                title="Чизбургер-пицца"
+                price="395"
+                imageUrl="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+              />
+              <PizzaBlock
+                title="Пепперони"
+                price="290"
+                imageUrl="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
